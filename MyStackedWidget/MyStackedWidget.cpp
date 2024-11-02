869e569738e9a71425e24c4b995641fe7fe8b5fe
WeiWang <1428206861@qq.com>
2024-11-02 21:31:04 +0800

    增加自定义堆栈窗口模块

---
+++ MyStackedWidget/MyStackedWidget.cpp
@@ -0,0 +1,12 @@
+//
+// Created by WeiWang on 24-11-2.
+//
+
+#include "MyStackedWidget.h"
+
+
+MyStackedWidget::MyStackedWidget(QWidget *parent) :
+    QStackedWidget(parent)
+{
+
+}
---
+++ MyStackedWidget/MyStackedWidget.h
@@ -0,0 +1,19 @@
+//
+// Created by WeiWang on 24-11-2.
+//
+
+#ifndef MYSTACKEDWIDGET_H
+#define MYSTACKEDWIDGET_H
+
+#include <QStackedWidget>
+
+class MyStackedWidget : public QStackedWidget {
+Q_OBJECT
+
+public:
+    explicit MyStackedWidget(QWidget *parent = nullptr);
+
+};
+
+
+#endif //MYSTACKEDWIDGET_H
