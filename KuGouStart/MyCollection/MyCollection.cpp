8570ba40ebfac370969ce53c890eea2711f83bdd
WeiWang <1428206861@qq.com>
2024-11-15 11:31:50 +0800

    成功搭建我的收藏堆栈窗口

--- KuGouStart/MyCollection/MyCollection.cpp
+++ KuGouStart/MyCollection/MyCollection.cpp
@@ -7,12 +7,94 @@
 #include "MyCollection.h"
 #include "ui_MyCollection.h"
 
+#include<QButtonGroup>
 
 MyCollection::MyCollection(QWidget *parent) :
-    QWidget(parent), ui(new Ui::MyCollection) {
+    QWidget(parent), ui(new Ui::MyCollection)
+    ,m_buttonGroup(std::make_unique<QButtonGroup>(this))
+{
     ui->setupUi(this);
+    initStackedWidget();
 }
 
 MyCollection::~MyCollection() {
     delete ui;
+}
+
+void MyCollection::initStackedWidget() {
+    initSingleSong();
+    initSongList();
+    initSpecialAlbum();
+    initCollectVideo();
+    initSinger();
+    initDevice();
+    //设置互斥
+    this->m_buttonGroup->addButton(ui->singleSong_pushButton);
+    this->m_buttonGroup->addButton(ui->songList_pushButton);
+    this->m_buttonGroup->addButton(ui->specialAlbum_pushButton);
+    this->m_buttonGroup->addButton(ui->collectVideo_pushButton);
+    this->m_buttonGroup->addButton(ui->singer_pushButton);
+    this->m_buttonGroup->addButton(ui->device_pushButton);
+    this->m_buttonGroup->setExclusive(true);
+}
+
+void MyCollection::initSingleSong() {
+    this->m_singleSong = std::make_unique<SingleSong>(ui->stackedWidget);
+    ui->stackedWidget->addWidget(this->m_singleSong.get());
+    ui->stackedWidget->setCurrentWidget(this->m_singleSong.get());
+}
+
+void MyCollection::initSongList() {
+    this->m_songList = std::make_unique<SongListWidget>(ui->stackedWidget);
+    ui->stackedWidget->addWidget(this->m_songList.get());
+}
+
+void MyCollection::initSpecialAlbum() {
+    this->m_specialAlbum = std::make_unique<SpecialAlbum>(ui->stackedWidget);
+    ui->stackedWidget->addWidget(this->m_specialAlbum.get());
+}
+
+void MyCollection::initCollectVideo() {
+    this->m_collectVideo = std::make_unique<CollectVideo>(ui->stackedWidget);
+    ui->stackedWidget->addWidget(this->m_collectVideo.get());
+}
+
+void MyCollection::initSinger() {
+    this->m_singerWidget = std::make_unique<SingerWidget>(ui->stackedWidget);
+    ui->stackedWidget->addWidget(this->m_singerWidget.get());
+}
+
+void MyCollection::initDevice() {
+    this->m_deviceWidget = std::make_unique<DeviceWidget>(ui->stackedWidget);
+    ui->stackedWidget->addWidget(this->m_deviceWidget.get());
+}
+
+void MyCollection::on_singleSong_pushButton_clicked() {
+    ui->stackedWidget->setCurrentWidget(this->m_singleSong.get());
+    qDebug()<<"点击singleSong";
+}
+
+void MyCollection::on_songList_pushButton_clicked() {
+    ui->stackedWidget->setCurrentWidget(this->m_songList.get());
+    qDebug()<<"点击songList";
+}
+
+void MyCollection::on_specialAlbum_pushButton_clicked() {
+    ui->stackedWidget->setCurrentWidget(this->m_specialAlbum.get());
+    qDebug()<<"点击specialAlbum";
+}
+
+void MyCollection::on_collectVideo_pushButton_clicked() {
+    ui->stackedWidget->setCurrentWidget(this->m_collectVideo.get());
+    qDebug()<<"点击collectVideo";
+}
+
+void MyCollection::on_singer_pushButton_clicked() {
+    ui->stackedWidget->setCurrentWidget(this->m_singerWidget.get());
+    qDebug()<<"点击singerWidget";
+}
+
+void MyCollection::on_device_pushButton_clicked() {
+    ui->stackedWidget->setCurrentWidget(this->m_deviceWidget.get());
+    qDebug()<<"点击deviceWidget";
 }
--- KuGouStart/MyCollection/MyCollection.h
+++ KuGouStart/MyCollection/MyCollection.h
@@ -5,8 +5,16 @@
 #ifndef MYCOLLECTION_H
 #define MYCOLLECTION_H
 
+#include"SingleSong.h"
+#include"SongListWidget.h"
+#include"SpecialAlbum.h"
+#include"CollectVideo.h"
+#include"SingerWidget.h"
+#include"DeviceWidget.h"
+
 #include <QWidget>
 
+class QButtonGroup;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MyCollection; }
@@ -17,10 +25,48 @@ Q_OBJECT
 
 public:
     explicit MyCollection(QWidget *parent = nullptr);
+
     ~MyCollection() override;
+
+    void initStackedWidget();
+
+    void initSingleSong();
+
+    void initSongList();
+
+    void initSpecialAlbum();
+
+    void initCollectVideo();
+
+    void initSinger();
+
+    void initDevice();
+
+private slots:
+    void on_singleSong_pushButton_clicked();
+
+    void on_songList_pushButton_clicked();
+
+    void on_specialAlbum_pushButton_clicked();
+
+    void on_collectVideo_pushButton_clicked();
+
+    void on_singer_pushButton_clicked();
+
+    void on_device_pushButton_clicked();
 
 private:
     Ui::MyCollection *ui;
+    std::unique_ptr<QButtonGroup>   m_buttonGroup{};
+    //堆栈窗口
+    std::unique_ptr<SingleSong>     m_singleSong{};
+    std::unique_ptr<SongListWidget> m_songList{};
+    std::unique_ptr<SpecialAlbum>   m_specialAlbum{};
+    std::unique_ptr<CollectVideo>   m_collectVideo{};
+    std::unique_ptr<SingerWidget>   m_singerWidget{};
+    std::unique_ptr<DeviceWidget>   m_deviceWidget{};
+
+
 };
 
 
