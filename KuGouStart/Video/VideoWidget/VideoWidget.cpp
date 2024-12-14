a5f5d1e7897f48c974bdad2873377f74552626e1
WeiWang <1428206861@qq.com>
2024-12-15 00:37:55 +0800

    剩余子部件待完善

--- KuGouStart/Video/VideoWidget/VideoWidget.cpp
+++ KuGouStart/Video/VideoWidget/VideoWidget.cpp
@@ -7,12 +7,107 @@
 #include "VideoWidget.h"
 #include "ui_VideoWidget.h"
 
+#include <QButtonGroup>
+#include <QFile>
 
-VideoWidget::VideoWidget(QWidget *parent) :
-    QWidget(parent), ui(new Ui::VideoWidget) {
+// 创建一个宏来截取 __FILE__ 宏中的目录部分
+#define GET_CURRENT_DIR (QString(__FILE__).first(qMax(QString(__FILE__).lastIndexOf('/'), QString(__FILE__).lastIndexOf('\\'))))
+
+
+VideoWidget::VideoWidget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::VideoWidget)
+    , m_buttonGroup(std::make_unique<QButtonGroup>(this))
+    , m_upBtn(std::make_unique<UpToolButton>(this))
+{
     ui->setupUi(this);
+    {
+        QFile file(GET_CURRENT_DIR + QStringLiteral("/video.css"));
+        if (file.open(QIODevice::ReadOnly)) {
+            this->setStyleSheet(file.readAll());
+        } else {
+            qDebug() << "样式表打开失败QAQ";
+            return;
+        }
+    }
+    initButtonGroup();
+    initTotalWidget();
+    initVector();
+    initUi();
 }
 
 VideoWidget::~VideoWidget() {
     delete ui;
+}
+
+void VideoWidget::initButtonGroup() const {
+    this->m_buttonGroup->addButton(ui->recommend_pushButton);
+    this->m_buttonGroup->addButton(ui->video_rank_pushButton);
+    this->m_buttonGroup->addButton(ui->MV_pushButton);
+    this->m_buttonGroup->addButton(ui->site_pushButton);
+    this->m_buttonGroup->addButton(ui->cover_pushButton);
+    this->m_buttonGroup->addButton(ui->dance_pushButton);
+    this->m_buttonGroup->addButton(ui->children_pushButton);
+    this->m_buttonGroup->addButton(ui->live_pushButton);
+    this->m_buttonGroup->addButton(ui->first_concert_pushButton);
+    this->m_buttonGroup->addButton(ui->chinese_language_pushButton);
+    this->m_buttonGroup->addButton(ui->South_Korea_pushButton);
+    this->m_buttonGroup->addButton(ui->Japan_pushButton);
+    this->m_buttonGroup->addButton(ui->American_pushButton);
+    this->m_buttonGroup->setExclusive(true);
+}
+
+void VideoWidget::initTotalWidget() const {
+
+}
+
+void VideoWidget::initVector() {
+}
+
+void VideoWidget::handleWheelValue(const int &value) {
+}
+
+void VideoWidget::onUpBtnClicked() {
+}
+
+void VideoWidget::onUpBtnShowOrNot() {
+}
+
+void VideoWidget::resizeEvent(QResizeEvent *event) {
+    QWidget::resizeEvent(event);
+}
+
+void VideoWidget::mousePressEvent(QMouseEvent *event) {
+    QWidget::mousePressEvent(event);
+}
+
+void VideoWidget::initUi() {
+    //设置ToolButton
+    ui->all_toolButton->setHoverFontColor(QColor(QStringLiteral("#3AA1FF")));
+    ui->all_toolButton->setApproach(true);
+    ui->all_toolButton->setIconSize(QSize(10,10));
+    ui->all_toolButton->setIcon(QIcon(QStringLiteral(":/ListenBook/Res/listenbook/up-gray.svg")));
+    ui->all_toolButton->setEnterIcon(QIcon(QStringLiteral(":/ListenBook/Res/listenbook/up-blue.svg")));
+    ui->all_toolButton->setLeaveIcon(QIcon(QStringLiteral(":/ListenBook/Res/listenbook/up-gray.svg")));
+    //设置title_widget2初始隐藏
+    ui->title_widget2->hide();
+}
+
+void VideoWidget::on_all_toolButton_toggled() {
+    if (ui->all_toolButton->isChecked()) {
+        ui->all_toolButton->setText(QStringLiteral("收起"));
+        ui->all_toolButton->setIcon(QIcon(QStringLiteral(":/ListenBook/Res/listenbook/down-blue.svg")));
+        ui->all_toolButton->setEnterIcon(QIcon(QStringLiteral(":/ListenBook/Res/listenbook/down-blue.svg")));
+        ui->all_toolButton->setLeaveIcon(QIcon(QStringLiteral(":/ListenBook/Res/listenbook/down-gray.svg")));
+        ui->title_widget2->show();
+        //update();
+    } else {
+        ui->all_toolButton->setText(QStringLiteral("全部"));
+        ui->all_toolButton->setIcon(QIcon(QStringLiteral(":/ListenBook/Res/listenbook/up-blue.svg")));
+        ui->all_toolButton->setEnterIcon(QIcon(QStringLiteral(":/ListenBook/Res/listenbook/up-blue.svg")));
+        ui->all_toolButton->setLeaveIcon(QIcon(QStringLiteral(":/ListenBook/Res/listenbook/up-gray.svg")));
+        ui->title_widget2->hide();
+        //update();
+    }
+
 }
--- KuGouStart/Video/VideoWidget/VideoWidget.h
+++ KuGouStart/Video/VideoWidget/VideoWidget.h
@@ -5,22 +5,58 @@
 #ifndef VIDEOWIDGET_H
 #define VIDEOWIDGET_H
 
+#include"UpToolButton.h"
+#include"VideoPartWidget.h"
 #include <QWidget>
 
+class QScrollBar;
+class QButtonGroup;
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class VideoWidget; }
+
+namespace Ui {
+    class VideoWidget;
+}
+
 QT_END_NAMESPACE
 
 class VideoWidget : public QWidget {
-Q_OBJECT
+    Q_OBJECT
 
 public:
     explicit VideoWidget(QWidget *parent = nullptr);
+
     ~VideoWidget() override;
 
 private:
+    void initButtonGroup()const;
+
+    void initTotalWidget()const;
+
+    void initUi();
+
+    void initVector();
+
+private slots:
+    void on_all_toolButton_toggled();
+
+    void handleWheelValue(const int& value);
+
+    void onUpBtnClicked();
+
+    void onUpBtnShowOrNot();
+
+protected:
+    void resizeEvent(QResizeEvent *event) override;
+
+    void mousePressEvent(QMouseEvent *event) override;
+
+private:
     Ui::VideoWidget *ui;
+    std::unique_ptr<QButtonGroup> m_buttonGroup{};
+    std::unique_ptr<UpToolButton> m_upBtn{};
+    //13个widget
+
 };
 
 
