5930338d71e5d08c8b50053d65dcde1d418eb1f8
WeiWang <1428206861@qq.com>
2025-01-31 22:00:30 +0800

    最近播放歌单界面完成

--- KuGouStart/RecentlyPlayed/RecentlySongList/RecentlySongList.cpp
+++ KuGouStart/RecentlyPlayed/RecentlySongList/RecentlySongList.cpp
@@ -7,10 +7,23 @@
 #include "RecentlySongList.h"
 #include "ui_RecentlySongList.h"
 
+#include <QFile>
+
+#define GET_CURRENT_DIR (QString(__FILE__).first(qMax(QString(__FILE__).lastIndexOf('/'), QString(__FILE__).lastIndexOf('\\'))))
 
 RecentlySongList::RecentlySongList(QWidget *parent) :
     QWidget(parent), ui(new Ui::RecentlySongList) {
     ui->setupUi(this);
+    {
+        QFile file(GET_CURRENT_DIR + QStringLiteral("/list.css"));
+        if (file.open(QIODevice::ReadOnly)) {
+            this->setStyleSheet(file.readAll());
+        } else {
+            qDebug() << "样式表打开失败QAQ";
+            return;
+        }
+    }
+
 }
 
 RecentlySongList::~RecentlySongList() {
