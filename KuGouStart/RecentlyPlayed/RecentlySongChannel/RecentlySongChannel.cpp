3575c6367b75910eadf332337917cca305c988d3
WeiWang <1428206861@qq.com>
2025-01-31 23:23:38 +0800

    最近播放音乐频道界面待完善

--- KuGouStart/RecentlyPlayed/RecentlySongChannel/RecentlySongChannel.cpp
+++ KuGouStart/RecentlyPlayed/RecentlySongChannel/RecentlySongChannel.cpp
@@ -6,13 +6,72 @@
 
 #include "RecentlySongChannel.h"
 #include "ui_RecentlySongChannel.h"
+#include "RecentlyChannelBlock.h"
 
+#include <QFile>
 
-RecentlySongChannel::RecentlySongChannel(QWidget *parent) :
-    QWidget(parent), ui(new Ui::RecentlySongChannel) {
+#define GET_CURRENT_DIR (QString(__FILE__).first(qMax(QString(__FILE__).lastIndexOf('/'), QString(__FILE__).lastIndexOf('\\'))))
+
+RecentlySongChannel::RecentlySongChannel(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::RecentlySongChannel)
+    , m_searchAction(new QAction(this))
+{
     ui->setupUi(this);
+    {
+        QFile file(GET_CURRENT_DIR + QStringLiteral("/song.css"));
+        if (file.open(QIODevice::ReadOnly)) {
+            this->setStyleSheet(file.readAll());
+        } else {
+            qDebug() << "样式表打开失败QAQ";
+            return;
+        }
+    }
+    initUi();
 }
 
 RecentlySongChannel::~RecentlySongChannel() {
     delete ui;
+}
+
+void RecentlySongChannel::initUi() {
+    ui->recently_play_toolButton->setIcon(QIcon(QStringLiteral(":/Res/tabIcon/play3-white.svg")));
+    //处理搜索栏
+    {
+        //使用 addAction 添加右侧图标
+        this->m_searchAction->setIcon(QIcon(QStringLiteral(":/Res/menuIcon/search-black.svg")));
+        this->m_searchAction->setIconVisibleInMenu(false); // 仅显示图标
+        ui->recently_search_lineEdit->addAction(this->m_searchAction, QLineEdit::TrailingPosition);
+        ui->recently_search_lineEdit->setWidth(150);
+        QToolButton* searchButton = nullptr;
+        foreach (QToolButton* btn, ui->recently_search_lineEdit->findChildren<QToolButton*>()) {
+            if (btn->defaultAction() == this->m_searchAction) {
+                searchButton = btn;
+                break;
+            }
+        }
+
+        // 安装事件过滤器
+        if (searchButton) {
+            searchButton->installEventFilter(this);
+        }
+    }
+    //处理RecentlyChannelBlock
+    const auto  lay = new QHBoxLayout(ui->table_widget);
+    const auto block = new RecentlyChannelBlock(ui->table_widget);
+    block->setCoverPix(QStringLiteral(":/Res/tabIcon/like.png"));
+    lay->addWidget(block);
+    lay->addStretch();
+}
+
+bool RecentlySongChannel::eventFilter(QObject *watched, QEvent *event) {
+    if (const auto button = qobject_cast<QToolButton*>(watched); button && button->defaultAction() == this->m_searchAction) {
+        if (event->type() == QEvent::Enter) {
+            this->m_searchAction->setIcon(QIcon(QStringLiteral(":/Res/menuIcon/search-blue.svg")));
+        } else if (event->type() == QEvent::Leave) {
+            this->m_searchAction->setIcon(QIcon(QStringLiteral(":/Res/menuIcon/search-black.svg")));
+        }
+    }
+
+    return QObject::eventFilter(watched, event);
 }
--- KuGouStart/RecentlyPlayed/RecentlySongChannel/RecentlySongChannel.h
+++ KuGouStart/RecentlyPlayed/RecentlySongChannel/RecentlySongChannel.h
@@ -9,18 +9,31 @@
 
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class RecentlySongChannel; }
+
+namespace Ui {
+    class RecentlySongChannel;
+}
+
 QT_END_NAMESPACE
 
 class RecentlySongChannel : public QWidget {
-Q_OBJECT
+    Q_OBJECT
 
 public:
     explicit RecentlySongChannel(QWidget *parent = nullptr);
+
     ~RecentlySongChannel() override;
 
 private:
+    void initUi();
+
+protected:
+    bool eventFilter(QObject *watched, QEvent *event) override;
+
+private:
     Ui::RecentlySongChannel *ui;
+    QAction *m_searchAction; //专门为了设置图片
+
 };
 
 
