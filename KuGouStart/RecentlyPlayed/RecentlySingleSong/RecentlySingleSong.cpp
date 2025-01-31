910cc45bce290e84c42e2e38006c1fe4d05fd0e3
WeiWang <1428206861@qq.com>
2025-01-31 22:00:00 +0800

    最近播放单曲界面完成

--- KuGouStart/RecentlyPlayed/RecentlySingleSong/RecentlySingleSong.cpp
+++ KuGouStart/RecentlyPlayed/RecentlySingleSong/RecentlySingleSong.cpp
@@ -7,12 +7,72 @@
 #include "RecentlySingleSong.h"
 #include "ui_RecentlySingleSong.h"
 
+#include <QFile>
 
-RecentlySingleSong::RecentlySingleSong(QWidget *parent) :
-    QWidget(parent), ui(new Ui::RecentlySingleSong) {
+#define GET_CURRENT_DIR (QString(__FILE__).first(qMax(QString(__FILE__).lastIndexOf('/'), QString(__FILE__).lastIndexOf('\\'))))
+
+RecentlySingleSong::RecentlySingleSong(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::RecentlySingleSong)
+    , m_searchAction(new QAction(this))
+{
     ui->setupUi(this);
+    
+    {
+        QFile file(GET_CURRENT_DIR + QStringLiteral("/single.css"));
+        if (file.open(QIODevice::ReadOnly)) {
+            this->setStyleSheet(file.readAll());
+        } else {
+            qDebug() << "样式表打开失败QAQ";
+            return;
+        }
+    }
+    initUi();
 }
 
 RecentlySingleSong::~RecentlySingleSong() {
     delete ui;
+}
+
+void RecentlySingleSong::initUi() {
+    ui->recently_play_toolButton->setIcon(QIcon(QStringLiteral(":/Res/tabIcon/play3-white.svg")));
+    ui->recently_download_toolButton->setIcon(QIcon(QStringLiteral(":/Res/tabIcon/download-gray.svg")));
+    ui->recently_download_toolButton->installEventFilter(this);
+
+    //使用 addAction 添加右侧图标
+    this->m_searchAction->setIcon(QIcon(QStringLiteral(":/Res/menuIcon/search-black.svg")));
+    this->m_searchAction->setIconVisibleInMenu(false); // 仅显示图标
+    ui->recently_search_lineEdit->addAction(this->m_searchAction, QLineEdit::TrailingPosition);
+    ui->recently_search_lineEdit->setWidth(150);
+    QToolButton* searchButton = nullptr;
+    foreach (QToolButton* btn, ui->recently_search_lineEdit->findChildren<QToolButton*>()) {
+        if (btn->defaultAction() == this->m_searchAction) {
+            searchButton = btn;
+            break;
+        }
+    }
+
+    // 安装事件过滤器
+    if (searchButton) {
+        searchButton->installEventFilter(this);
+    }
+}
+
+bool RecentlySingleSong::eventFilter(QObject *watched, QEvent *event) {
+    if (watched == ui->recently_download_toolButton) {
+        if (event->type() == QEvent::Enter) {
+            ui->recently_download_toolButton->setIcon(QIcon(QStringLiteral(":/Res/menuIcon/download-blue.svg")));
+        } else if (event->type() == QEvent::Leave) {
+            ui->recently_download_toolButton->setIcon(QIcon(QStringLiteral(":/Res/tabIcon/download-gray.svg")));
+        }
+    }
+    if (const auto button = qobject_cast<QToolButton*>(watched); button && button->defaultAction() == this->m_searchAction) {
+        if (event->type() == QEvent::Enter) {
+            this->m_searchAction->setIcon(QIcon(QStringLiteral(":/Res/menuIcon/search-blue.svg")));
+        } else if (event->type() == QEvent::Leave) {
+            this->m_searchAction->setIcon(QIcon(QStringLiteral(":/Res/menuIcon/search-black.svg")));
+        }
+    }
+
+    return QObject::eventFilter(watched, event);
 }
--- KuGouStart/RecentlyPlayed/RecentlySingleSong/RecentlySingleSong.h
+++ KuGouStart/RecentlyPlayed/RecentlySingleSong/RecentlySingleSong.h
@@ -9,19 +9,30 @@
 
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class RecentlySingleSong; }
+
+namespace Ui {
+    class RecentlySingleSong;
+}
+
 QT_END_NAMESPACE
 
 class RecentlySingleSong : public QWidget {
-Q_OBJECT
+    Q_OBJECT
 
 public:
     explicit RecentlySingleSong(QWidget *parent = nullptr);
+
     ~RecentlySingleSong() override;
 
 private:
+    void initUi();
+
+protected:
+    bool eventFilter(QObject *watched, QEvent *event) override;
+
+private:
     Ui::RecentlySingleSong *ui;
+    QAction *m_searchAction; //专门为了设置图片
+
 };
-
-
 #endif //RECENTLYSINGLESONG_H
