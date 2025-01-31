7c91398e9a24d56e204df62cadf5a4a31ebb09d7
WeiWang <1428206861@qq.com>
2025-01-31 21:59:47 +0800

    最近播放视频界面完成

--- KuGouStart/RecentlyPlayed/RecentlyVideoWidget/RecentlyVideoWidget.cpp
+++ KuGouStart/RecentlyPlayed/RecentlyVideoWidget/RecentlyVideoWidget.cpp
@@ -7,10 +7,22 @@
 #include "RecentlyVideoWidget.h"
 #include "ui_RecentlyVideoWidget.h"
 
+#include <QFile>
+
+#define GET_CURRENT_DIR (QString(__FILE__).first(qMax(QString(__FILE__).lastIndexOf('/'), QString(__FILE__).lastIndexOf('\\'))))
 
 RecentlyVideoWidget::RecentlyVideoWidget(QWidget *parent) :
     QWidget(parent), ui(new Ui::RecentlyVideoWidget) {
     ui->setupUi(this);
+    {
+        QFile file(GET_CURRENT_DIR + QStringLiteral("/video.css"));
+        if (file.open(QIODevice::ReadOnly)) {
+            this->setStyleSheet(file.readAll());
+        } else {
+            qDebug() << "样式表打开失败QAQ";
+            return;
+        }
+    }
 }
 
 RecentlyVideoWidget::~RecentlyVideoWidget() {
