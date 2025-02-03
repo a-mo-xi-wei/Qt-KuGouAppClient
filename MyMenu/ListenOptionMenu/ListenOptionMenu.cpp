b23eefb5da7396674ac5f028cc016c2829a54f2d
WeiWang <1428206861@qq.com>
2025-02-03 09:35:54 +0800

    添加子组件

---
+++ MyMenu/ListenOptionMenu/ListenOptionMenu.cpp
@@ -0,0 +1,13 @@
+//
+// Created by WeiWang on 25-2-3.
+//
+
+#include "ListenOptionMenu.h"
+
+
+ListenOptionMenu::ListenOptionMenu(QWidget *parent)
+    : BaseMenu(parent)
+{
+
+}
+
---
+++ MyMenu/ListenOptionMenu/ListenOptionMenu.h
@@ -0,0 +1,19 @@
+//
+// Created by WeiWang on 25-2-3.
+//
+
+#ifndef LISTENOPTIONMENU_H
+#define LISTENOPTIONMENU_H
+
+#include "../BaseMenu.h"
+
+class ListenOptionMenu : public BaseMenu {
+Q_OBJECT
+
+public:
+    explicit ListenOptionMenu(QWidget *parent = nullptr);
+
+};
+
+
+#endif //LISTENOPTIONMENU_H
