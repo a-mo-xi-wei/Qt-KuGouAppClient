c06fcb023fe393dc9a5c64cd1492b1b3ae52b515
WeiWang <1428206861@qq.com>
2025-02-04 11:02:31 +0800

    重构听书模块中我的下载

--- KuGouStart/ListenBook/ListenMyDownload/ListenMyDownload.cpp
+++ KuGouStart/ListenBook/ListenMyDownload/ListenMyDownload.cpp
@@ -7,12 +7,192 @@
 #include "ListenMyDownload.h"
 #include "ui_ListenMyDownload.h"
 
+#include <QFile>
 
-ListenMyDownload::ListenMyDownload(QWidget *parent) :
-    QWidget(parent), ui(new Ui::ListenMyDownload) {
+#include <QButtonGroup>
+#include <QMouseEvent>
+
+
+#define GET_CURRENT_DIR (QString(__FILE__).first(qMax(QString(__FILE__).lastIndexOf('/'), QString(__FILE__).lastIndexOf('\\'))))
+
+ListenMyDownload::ListenMyDownload(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::ListenMyDownload)
+    , m_buttonGroup(std::make_unique<QButtonGroup>(this))
+{
     ui->setupUi(this);
+    {
+        QFile file(GET_CURRENT_DIR + QStringLiteral("/download.css"));
+        if (file.open(QIODevice::ReadOnly)) {
+            this->setStyleSheet(file.readAll());
+        }
+        else {
+            qDebug() << "样式表打开失败QAQ";
+            return;
+        }
+    }
+    initUi();
 }
 
 ListenMyDownload::~ListenMyDownload() {
     delete ui;
+}
+
+void ListenMyDownload::initStackedWidget() {
+    initDownloadedWidget();
+    initDownloadingWidget();
+    //设置互斥
+    this->m_buttonGroup->addButton(ui->downloaded_pushButton);
+    this->m_buttonGroup->addButton(ui->downloading_pushButton);
+    this->m_buttonGroup->setExclusive(true);
+}
+
+void ListenMyDownload::initUi() {
+    initStackedWidget();
+    initIndexLab();
+    ui->downloaded_pushButton->clicked();
+}
+
+void ListenMyDownload::initIndexLab() {
+    //下标图片
+    ui->idx1_lab->setPixmap(QPixmap(QStringLiteral(":/Res/window/index_lab.svg")));
+    ui->idx2_lab->setPixmap(QPixmap(QStringLiteral(":/Res/window/index_lab.svg")));
+    ui->downloaded_number_label->setStyleSheet(QStringLiteral("color:#26a1ff;font-size:16px;font-weight:bold;"));
+    ui->idx2_lab->hide();
+    //同步状态
+    ui->guide_widget1->installEventFilter(this);
+    ui->guide_widget2->installEventFilter(this);
+}
+
+void ListenMyDownload::initDownloadedWidget() {
+    this->m_downloaded = std::make_unique<DownloadedWidget>(ui->stackedWidget);
+    ui->stackedWidget->addWidget(this->m_downloaded.get());
+    ui->stackedWidget->setCurrentWidget(this->m_downloaded.get());
+}
+
+void ListenMyDownload::initDownloadingWidget() {
+    this->m_downloading = std::make_unique<DownloadingWidget>(ui->stackedWidget);
+    ui->stackedWidget->addWidget(this->m_downloading.get());
+}
+
+bool ListenMyDownload::eventFilter(QObject *watched, QEvent *event) {
+    if (watched == ui->guide_widget1) {
+        if (event->type() == QEvent::Enter) {
+            ui->downloaded_pushButton->setStyleSheet(R"(
+                QPushButton {
+                    color:#26a1ff;
+                    font-size:16px;
+                    border: none;
+                    padding: 0px;
+                    margin: 0px;
+                }
+                QPushButton:checked {
+                    color:#26a1ff;
+                    font-size:18px;
+                    font-weight:bold;
+                }
+            )");
+            if (ui->downloaded_pushButton->isChecked())
+                ui->downloaded_number_label->setStyleSheet(QStringLiteral("color:#26a1ff;font-size:16px;font-weight:bold;"));
+            else ui->downloaded_number_label->setStyleSheet(QStringLiteral("color:#26a1ff;"));
+        }
+        else if (event->type() == QEvent::Leave) {
+            ui->downloaded_pushButton->setStyleSheet(R"(
+                QPushButton {
+                    color:black;
+                    font-size:16px;
+                    border: none;
+                    padding: 0px;
+                    margin: 0px;
+                }
+                QPushButton:checked {
+                    color:#26a1ff;
+                    font-size:18px;
+                    font-weight:bold;
+                }
+            )");
+            if (ui->downloaded_pushButton->isChecked())
+                ui->downloaded_number_label->setStyleSheet(QStringLiteral("color:#26a1ff;font-size:16px;font-weight:bold;"));
+            else ui->downloaded_number_label->setStyleSheet("");
+        }
+    }
+    if (watched == ui->guide_widget2) {
+        if (event->type() == QEvent::Enter) {
+            ui->downloading_pushButton->setStyleSheet(R"(
+                QPushButton {
+                    color:#26a1ff;
+                    font-size:16px;
+                    border: none;
+                    padding: 0px;
+                    margin: 0px;
+                }
+                QPushButton:checked {
+                    color:#26a1ff;
+                    font-size:18px;
+                    font-weight:bold;
+                }
+            )");
+            if (ui->downloading_pushButton->isChecked())
+                ui->downloading_number_label->setStyleSheet(QStringLiteral("color:#26a1ff;font-size:16px;font-weight:bold;"));
+            else ui->downloading_number_label->setStyleSheet(QStringLiteral("color:#26a1ff;"));
+        }
+        else if (event->type() == QEvent::Leave) {
+            ui->downloading_pushButton->setStyleSheet(R"(
+                QPushButton {
+                    color:black;
+                    font-size:16px;
+                    border: none;
+                    padding: 0px;
+                    margin: 0px;
+                }
+                QPushButton:checked {
+                    color:#26a1ff;
+                    font-size:18px;
+                    font-weight:bold;
+                }
+            )");
+            if (ui->downloading_pushButton->isChecked())
+                ui->downloading_number_label->setStyleSheet(QStringLiteral("color:#26a1ff;font-size:16px;font-weight:bold;"));
+            else ui->downloading_number_label->setStyleSheet("");
+        }
+    }
+    return QWidget::eventFilter(watched, event);
+}
+
+void ListenMyDownload::mousePressEvent(QMouseEvent *event) {
+    QWidget::mousePressEvent(event);
+    if (event->button() == Qt::LeftButton) {
+        const auto labelRect1 = ui->downloaded_number_label     ->geometry();
+        const auto labelRect2 = ui->downloading_number_label       ->geometry();
+        // 将点击坐标转换为标签父控件的坐标系
+        const QPoint clickPos1 = ui->downloaded_number_label    ->parentWidget()->mapFrom(this, event->pos());
+        const QPoint clickPos2 = ui->downloading_number_label      ->parentWidget()->mapFrom(this, event->pos());
+
+        if (labelRect1.contains(clickPos1)) {
+            ui->downloaded_pushButton->clicked();
+        }
+        if (labelRect2.contains(clickPos2)) {
+            ui->downloading_pushButton->clicked();
+        }
+    }
+}
+
+void ListenMyDownload::on_downloaded_pushButton_clicked() {
+    if (ui->stackedWidget->currentWidget() == this->m_downloaded.get())return;
+    ui->downloaded_pushButton->setChecked(true);
+    ui->stackedWidget->setCurrentWidget(this->m_downloaded.get());
+    ui->idx1_lab->show();
+    ui->idx2_lab->hide();
+    ui->downloaded_number_label->setStyleSheet(QStringLiteral("color:#26a1ff;font-size:16px;font-weight:bold;"));
+    ui->downloading_number_label->setStyleSheet("");
+}
+
+void ListenMyDownload::on_downloading_pushButton_clicked() {
+    if (ui->stackedWidget->currentWidget() == this->m_downloading.get())return;
+    ui->downloading_pushButton->setChecked(true);
+    ui->stackedWidget->setCurrentWidget(this->m_downloading.get());
+    ui->idx1_lab->hide();
+    ui->idx2_lab->show();
+    ui->downloaded_number_label->setStyleSheet("");
+    ui->downloading_number_label->setStyleSheet(QStringLiteral("color:#26a1ff;font-size:16px;font-weight:bold;"));
 }
--- KuGouStart/ListenBook/ListenMyDownload/ListenMyDownload.h
+++ KuGouStart/ListenBook/ListenMyDownload/ListenMyDownload.h
@@ -5,22 +5,56 @@
 #ifndef LISTENMYDOWNLOAD_H
 #define LISTENMYDOWNLOAD_H
 
+#include "DownloadedWidget.h"
+#include "DownloadingWidget.h"
+
 #include <QWidget>
 
+class QButtonGroup;
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class ListenMyDownload; }
+
+namespace Ui {
+    class ListenMyDownload;
+}
+
 QT_END_NAMESPACE
 
 class ListenMyDownload : public QWidget {
-Q_OBJECT
+    Q_OBJECT
 
 public:
     explicit ListenMyDownload(QWidget *parent = nullptr);
+
     ~ListenMyDownload() override;
 
 private:
+    void initStackedWidget();
+
+    void initUi();
+
+    void initIndexLab();
+
+    void initDownloadedWidget();
+
+    void initDownloadingWidget();
+
+protected:
+    bool eventFilter(QObject *watched, QEvent *event) override;
+
+    void mousePressEvent(QMouseEvent *event) override;
+
+private slots:
+    void on_downloaded_pushButton_clicked();
+
+    void on_downloading_pushButton_clicked();
+
+private:
     Ui::ListenMyDownload *ui;
+    std::unique_ptr<QButtonGroup>   m_buttonGroup{};
+    //堆栈窗口
+    std::unique_ptr<DownloadedWidget>   m_downloaded{};
+    std::unique_ptr<DownloadingWidget>  m_downloading{};
 };
 
 
