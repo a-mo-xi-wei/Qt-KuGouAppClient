c6fb22f594623188be539d82f5f548489012188b
WeiWang <1428206861@qq.com>
2025-02-23 09:49:31 +0800

    打印调试

--- KuGouStart/main.cpp
+++ KuGouStart/main.cpp
@@ -1,9 +1,14 @@
 #include "KuGouApp.h"
 
 #include <QApplication>
-
+//void myMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
+//    QByteArray localMsg = msg.toLocal8Bit();
+//    fprintf(stderr, "[Qt Message] %s (File: %s, Line: %d, Function: %s)\n",
+//            localMsg.constData(), context.file, context.line, context.function);
+//}
 int main(int argc, char *argv[])
 {
+    //qInstallMessageHandler(myMessageHandler);
     QApplication a(argc, argv);
     KuGouApp w;
     w.show();
