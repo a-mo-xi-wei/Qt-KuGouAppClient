9ca35dcc7895b31161f0c639ff6930f5e12e719c
WeiWang <1428206861@qq.com>
2025-03-07 16:04:37 +0800

    添加配置文件模块

---
+++ util/MyConfigFile/MyConfigFile.cpp
@@ -0,0 +1,34 @@
+﻿#include "MyConfigFile.h"
+
+MyConfigFile::MyConfigFile(const QString& filename)
+	:m_settings(filename,QSettings::IniFormat)
+{
+}
+
+void MyConfigFile::setValue(QAnyStringView key, const QVariant& value)
+{
+	m_settings.setValue(key, value);
+}
+
+QVariant MyConfigFile::value(QAnyStringView key) const
+{
+	return value(key,QVariant());
+}
+
+QVariant MyConfigFile::value(QAnyStringView key, const QVariant& def_value) const
+{
+#ifdef _DEBUG
+	auto v = m_settings.value(key);
+	if (!v.isValid())
+	{
+		qWarning() << key.toString() << "Not Found";
+		return def_value;
+	}
+#endif // _DEBUG
+	return m_settings.value(key,def_value);//key存在就返回对应的value,否则返回def_value
+}  
+
+void MyConfigFile::removeValue(QAnyStringView key)
+{
+	m_settings.remove(key);
+}
---
+++ util/MyConfigFile/MyConfigFile.h
@@ -0,0 +1,27 @@
+﻿#ifndef MYCONFIGFILE_H_
+#define MYCONFIGFILE_H_
+
+#include <QSettings>
+#include <QAnyStringView>
+
+#if defined(MYCONFIG_LIBRARY)
+#define MYCONFIG_EXPORT Q_DECL_EXPORT
+#else
+#define MYCONFIG_EXPORT Q_DECL_IMPORT
+#endif
+
+class MYCONFIG_EXPORT MyConfigFile
+{
+public:
+	MyConfigFile(const QString& filename);
+	bool isOpen()const { return m_settings.status() == QSettings::NoError; }
+
+	void setValue(QAnyStringView key, const QVariant& value);
+	QVariant value(QAnyStringView key)const;
+	QVariant value(QAnyStringView key,const QVariant& def_value)const;
+	void removeValue(QAnyStringView key);
+private:
+	QSettings m_settings;
+};
+
+#endif // !MYCONFIGFILE_H_
