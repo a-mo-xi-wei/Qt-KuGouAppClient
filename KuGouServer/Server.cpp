44f63a4a9907a3dd237df8d252a105c2973e9220
WeiWang <1428206861@qq.com>
2025-03-14 09:59:42 +0800

    优化服务端代码

--- KuGouServer/Server.cpp
+++ KuGouServer/Server.cpp
@@ -6,10 +6,14 @@
 
 Server::Server() {
     initDateBase();
-
+    initRouter();
+    // 初始日志系统，设置日志文件路径
+    init_log_file(QCoreApplication::applicationDirPath() + QString("/logs/serve_%1.log").arg(QDateTime::currentDateTime().toString("yyyy-MM-dd")));
     m_httpserver.setNetworkFrameManager(this);
-    m_httpserver.listen(8080);
-
+    if (!m_httpserver.isRunning()) {
+        m_httpserver.listen(8080);
+        QLOG_INFO()<< QDateTime::currentDateTime().toString("[yyyy-MM-dd hh:mm:ss] ") + "服务器启动成功";
+    }
 }
 
 void Server::initDateBase() {
@@ -49,36 +53,66 @@ void Server::initDateBase() {
 
 }
 
+void Server::initRouter() {
+    apiRouter["/api/test"] = &Server::onApiTest;
+    apiRouter["/api/version"] = &Server::onApiVersion;
+    apiRouter["/api/addSong"] = &Server::onApiAddSong;
+    apiRouter["/api/delSong"] = &Server::onApiDelSong;
+}
+
 bool Server::OnProcessHttpAccepted(QObject *obj, const QPointer<JQHttpServer::Session> &session) {
-    qDebug()<<"看到我，你就有了";
+    //qDebug()<<"看到我，你就有了";
     QString path = session->requestUrl();
     QString method = session->requestMethod(); // GET/POST/PUT/DELETE
-    QMap<QString,QString> header = session->requestUrlQuery();
+    //QMap<QString,QString> header = session->requestUrlQuery();
 
     bool isProcessed = false;
 
     if(obj == &m_httpserver)
     {
-        //session->replyBytes(prequestUrl.toUtf8(),"text/html; charset=UTF-8");
         // 2. 根据路径和方法处理请求
-        if (path == "/api/test" && method == "GET") {
-            // 解析请求数据（假设是 JSON）
-            QJsonDocument requestDoc = QJsonDocument::fromJson(session->requestBody());
-            QJsonObject requestData = requestDoc.object();
-
-            // 返回成功响应
-            QJsonObject response;
-            response["status"] = "success";
-            session->replyBytes(QJsonDocument(response).toJson(),"application/json");
-            isProcessed = true;
+        if (method == "GET") {
+            if (apiRouter.contains(path)) {
+                isProcessed = apiRouter[path](session);
+            }
         }
+
     }
 
-
-    QDateTime dateTime =QDateTime::currentDateTime();
-    qDebug()<< dateTime.toString("[yyyy-MM-dd hh:mm:ss] ")+
+    QLOG_INFO()<< QDateTime::currentDateTime().toString("[yyyy-MM-dd hh:mm:ss] ")+
                       "Server::OnProcessHttpAccepted:"+
                       session->requestSourceIp();
 
     return isProcessed ? isProcessed : NetworkFrameManager::OnProcessHttpAccepted(obj,session);
+}
+
+bool Server::onApiTest(const QPointer<JQHttpServer::Session> &session) {
+    // 解析请求数据（假设是 JSON）
+    //QJsonDocument requestDoc = QJsonDocument::fromJson(session->requestBody());
+    //QJsonObject requestData = requestDoc.object();
+
+    // 返回成功响应
+    QJsonObject response;
+    response["status"] = "success";
+    session->replyBytes(QJsonDocument(response).toJson(),"application/json");
+    return true;
+}
+
+bool Server::onApiVersion(const QPointer<JQHttpServer::Session> &session) {
+    QJsonObject response;
+    response["version"] = "1.0";
+    session->replyBytes(QJsonDocument(response).toJson(),"application/json");
+    return true;
+}
+
+bool Server::onApiSearchSong(const QPointer<JQHttpServer::Session> &session) {
+    return false;
+}
+
+bool Server::onApiAddSong(const QPointer<JQHttpServer::Session> &session) {
+    return false;
+}
+
+bool Server::onApiDelSong(const QPointer<JQHttpServer::Session> &session) {
+    return false;
 }
--- KuGouServer/Server.h
+++ KuGouServer/Server.h
@@ -16,12 +16,30 @@ public:
 private:
     void initDateBase();
 
+    void initRouter();
+
     // 处理http请求
     bool OnProcessHttpAccepted(QObject *obj, const QPointer<JQHttpServer::Session> &session) override;
 
 private:
+    //处理各种API
+    //测试
+    static bool onApiTest(const QPointer<JQHttpServer::Session> &session);
+    //版本
+    static bool onApiVersion(const QPointer<JQHttpServer::Session> &session);
+    //搜索歌曲
+    static bool onApiSearchSong(const QPointer<JQHttpServer::Session> &session);
+    //添加歌曲
+    static bool onApiAddSong(const QPointer<JQHttpServer::Session> &session);
+    //删除歌曲
+    static bool onApiDelSong(const QPointer<JQHttpServer::Session> &session);
+
+private:
     chttpserver m_httpserver;
     SqliteDataProvider m_SqliteDataProvider;
+    //定义路由表
+    using ApiHandler = std::function<bool(const QPointer<JQHttpServer::Session> &)>;
+    QHash<QString, ApiHandler> apiRouter;
 };
 
 #endif //SERVER_H
