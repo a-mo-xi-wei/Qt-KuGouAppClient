d6aeff6e61a44057d8fbc4e6cd6e7fd31643735c
WeiWang <1428206861@qq.com>
2025-03-19 22:06:11 +0800

    加快头像图标缩放速度

--- KuGouStart/KuGouApp.cpp
+++ KuGouStart/KuGouApp.cpp
@@ -574,13 +574,13 @@ bool KuGouApp::eventFilter(QObject *watched, QEvent *event) {
 
         // 缩小动画
         auto *shrink = new QPropertyAnimation(ui->title_portrait_label, "size");
-        shrink->setDuration(300);
+        shrink->setDuration(200);
         shrink->setStartValue(originalSize);
         shrink->setEndValue(originalSize * 0.7);
 
         // 放大动画
         auto *expand = new QPropertyAnimation(ui->title_portrait_label, "size");
-        expand->setDuration(300);
+        expand->setDuration(200);
         expand->setStartValue(originalSize * 0.7);
         expand->setEndValue(originalSize);
 
