1bf47281619f691dc1bb12e9576774f8e3e1f902
WeiWang <1428206861@qq.com>
2025-07-30 10:27:32 +0800

    启用高 DPI 缩放支持

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 
 int main(int argc, char *argv[])
 {
+    // enable dpi scale
+#if (QT_VERSION > QT_VERSION_CHECK(5, 14, 0))
+    QApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+#endif
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
+
     QApplication a(argc, argv);
     AppController::instance().start();
     return a.exec();
